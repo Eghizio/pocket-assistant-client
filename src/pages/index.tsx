bad3bb8be15cf0b994d7d104857d18484af9f17a
raxonpc <tomeczekorlowski1@gmail.com>
2023-06-19 17:35:02 +0200

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { List, ListItem, Sheet } from "@mui/joy";
 import { NavBar } from "@/components/NavBar/NavBar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
