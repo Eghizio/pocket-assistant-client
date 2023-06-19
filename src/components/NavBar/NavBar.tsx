74c88e053df8a4b9fae9c70c8d9380d52606512a
raxonpc <75646062+raxonpc@users.noreply.github.com>
2023-06-19 17:33:25 +0200

    UI Library (Joy UI) (#1)
    * Set up joyui with dummy component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,36 @@
+import { Link, List, ListItem } from "@mui/joy";
+import NextLink from "next/link";
+
+export function NavBar() {
+    return (
+        <header>
+            <nav className="navigation">
+                <List orientation="horizontal">
+                    <ListItem>
+                        <NextLink href="/dashboard">
+                            <Link
+                                color="primary"
+                                variant="solid"
+                                component="button"
+                            >
+                                Dashboard
+                            </Link>
+                        </NextLink>
+                    </ListItem>
+
+                    <ListItem>
+                        <NextLink href="/signout">
+                            <Link
+                                color="neutral"
+                                variant="solid"
+                                component="button"
+                            >
+                                Sign out
+                            </Link>
+                        </NextLink>
+                    </ListItem>
+                </List>
+            </nav>
+        </header>
+    );
+}
