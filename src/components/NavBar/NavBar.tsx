0b69c13208276510332aae5ddfc1c739c98f4ed8
raxonpc <tomeczekorlowski1@gmail.com>
2023-06-19 17:58:54 +0200

    Create basic README

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,27 +2,35 @@ import { Link, List, ListItem } from "@mui/joy";
 import NextLink from "next/link";
 
 export function NavBar() {
-  return (
-    <header>
-      <nav>
-        <List orientation="horizontal">
-          <ListItem>
-            <NextLink href="/dashboard">
-              <Link color="primary" variant="solid" component="button">
-                Dashboard
-              </Link>
-            </NextLink>
-          </ListItem>
+    return (
+        <header>
+            <nav>
+                <List orientation="horizontal">
+                    <ListItem>
+                        <NextLink href="/dashboard">
+                            <Link
+                                color="primary"
+                                variant="solid"
+                                component="button"
+                            >
+                                Dashboard
+                            </Link>
+                        </NextLink>
+                    </ListItem>
 
-          <ListItem>
-            <NextLink href="/signout">
-              <Link color="neutral" variant="solid" component="button">
-                Sign out
-              </Link>
-            </NextLink>
-          </ListItem>
-        </List>
-      </nav>
-    </header>
-  );
+                    <ListItem>
+                        <NextLink href="/signout">
+                            <Link
+                                color="neutral"
+                                variant="solid"
+                                component="button"
+                            >
+                                Sign out
+                            </Link>
+                        </NextLink>
+                    </ListItem>
+                </List>
+            </nav>
+        </header>
+    );
 }
